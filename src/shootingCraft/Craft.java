0dcf50ffa324ed70e828db4d26e7ce79e91f46f6
Ahmad <ahmad.mohamad.cs@gmail.com>
2024-02-28 11:43:50 +0300

    Modifiy class craft.java and add missile image to static file

--- src/shootingCraft/Craft.java
+++ src/shootingCraft/Craft.java
@@ -1,9 +1,10 @@
 package shootingCraft;
 
 
-import java.awt.Image;
+import java.awt.*;
 import javax.swing.ImageIcon;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Craft {
     private int x;
@@ -11,6 +12,12 @@ public class Craft {
     private int dx;
     private int dy;
     private final Image image;
+
+    int width;
+    int height;
+    boolean visible;
+
+    private ArrayList<Missile> missiles;
 
 
 
@@ -20,7 +27,13 @@ public class Craft {
         image = imageIcon.getImage();
         x = 160;
         y = 160;
+
+        height = image.getHeight(null);
+        width = image.getWidth(null);
+        visible = true;
+        missiles = new ArrayList<Missile>();
     }
+
 
     public Image getImage() {
         return image;
@@ -40,6 +53,10 @@ public class Craft {
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_SPACE) {
+            fire
+        }
 
         if (key == KeyEvent.VK_LEFT) {
             if (x >= 0)
@@ -91,4 +108,25 @@ public class Craft {
         }
     }
 
+    public Rectangle getBound(){
+        return new Rectangle(x,y,width,height);
+    }
+    public ArrayList<Missile> getMissiles() {
+        return missiles;
+    }
+    public boolean isVisible(){
+        return this.visible;
+    }
+    public void setVisible(boolean bol)
+    {
+        this.visible = bol;
+    }
+    public void addMissile() {
+        Missile missile = new Missile();
+        missile.addMissile(x+width, y+height/2);
+    }
+
+    public void setMissiles(ArrayList<Missile> missiles) {
+        this.missiles = missiles;
+    }
 }
